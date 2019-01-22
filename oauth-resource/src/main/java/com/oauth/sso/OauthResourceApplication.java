61d9ee18520d8ecf86f7d6da284140c68d837e1a
xuh <xuh@fitme.ai>
2019-01-22 14:26:18 +0800

    创建项目

---
+++ oauth-resource/src/main/java/com/oauth/sso/OauthResourceApplication.java
@@ -0,0 +1,14 @@
+package com.oauth.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthResourceApplication.class, args);
+    }
+
+}
+
---
+++ oauth-soo/src/main/java/com/oauth/soo/OauthSooApplication.java
@@ -0,0 +1,14 @@
+package com.oauth.soo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthSooApplication.class, args);
+    }
+
+}
+
